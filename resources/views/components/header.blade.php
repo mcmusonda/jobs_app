4ed7236e460fdd119adb95260e01589eaf8658d3
Chali Musonda <mcmusonda@gmail.com>
2025-04-11 14:06:30 +0200

    tailwindcss installation & config

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="container mx-auto p-4 mt-4">
     <a href="/">Home</a>
     <a href="/jobs">Jobs</a>
     <a href="/jobs/create">Create Job</a>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Workopia | Find and List jobs' }}</title>
+    @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100">
+    
     <x-header />
 
     <main class="container mx-auto p-4 mt-4">

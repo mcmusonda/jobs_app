9e24358ca0ccb770219b83319a8b3bfd8c82a465
Chali Musonda <mcmusonda@gmail.com>
2025-04-16 16:12:19 +0200

    Hero Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,11 +7,14 @@
     <title>{{ $title ?? 'Workopia | Find and List jobs' }}</title>
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body class="bg-gray-100">
     
     <x-header />
-
+    @if(request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>

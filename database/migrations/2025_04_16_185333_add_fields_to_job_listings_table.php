f963cceafcd0d23dfca95c0e25a352dc612a5e2e
Chali Musonda <mcmusonda@gmail.com>
2025-04-16 23:46:53 +0200

    Job Schema Update

--- database/migrations/2025_04_16_185333_add_fields_to_job_listings_table.php
+++ database/migrations/2025_04_16_185333_add_fields_to_job_listings_table.php
@@ -15,9 +15,21 @@ return new class extends Migration
         DB:table('job_listings')->truncate();
         Schema::table('job_listings', function (Blueprint $table) {
             $table->integer('salary');
-            $table->string('tag');
-            $table->enum('job_type', ['Full-Time'. 'Part-Time','Contract', 'Temporary'])->default('Full-Time');
-            $table->loolean('remote')->default(false);
+            $table->string('tag')->nullable();
+            $table->enum('job_type', ['Full-Time'. 'Part-Time','Contract', 'Temporary', 'Internship', 'Volunteer', 'On-Call'])->default('Full-Time');
+            $table->boolean('remote')->default(false);
+            $table->string('requirements')->nullable();
+            $table->string('benefits')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode')->nullable();
+            $table->string('contact_email');
+            $table->string('contact_phone')->nullable();
+            $table->string('company_name');
+            $table->string('company_description')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('company_website')->nullable();
         });
     }
 
@@ -27,7 +39,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('job_listings', function (Blueprint $table) {
-            //
+            $table->dropColumn(['salary', 'tags', 'job_type', 'remote', 'requirements', 'benefits', 'address', 'city', 'state', 'zipcode', 'contact_email', 'contact_phone', 'company_name', 'company_description', 'company_logo', 'company_website']);
         });
     }
 };
